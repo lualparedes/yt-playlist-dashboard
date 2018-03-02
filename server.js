8a24301f3e2f3d8ce79f8a1c1f6729291dede387
Lual <lualps94@gmail.com>
2018-03-01 21:32:08 -0400

    Live reload configured

--- server.js
+++ server.js
@@ -1,9 +1,16 @@
 const express = require('express');
+const browserSync = require('browser-sync');
 const app = express();
-const port = process.env.PORT || 8000;
+const port = process.env.PORT || 3000;
 
 app.listen(port);
 
 app.get('/', (req, res) => {
-    res.sendFile('index.html',  {root : __dirname + '/public'});
+    res.sendFile('index.html',  {root : __dirname + '/app'});
+});
+
+browserSync({
+    server: 'app',
+    files: ["app/*.html", "app/css/*.css"],
+    open: false
 });
