e64ce261a92d03b8e8d84723d628a6a60dbfdde1
Lual <lualps94@gmail.com>
2018-03-09 12:55:03 -0400

    Initial styles added

--- README.md
+++ README.md
@@ -5,6 +5,8 @@ A dashboard with enhanced features for using YouTube playlists
 ## Features
 
 - D3 chart displaying the duration of each video
+- World-class interaction design (maybe use Tweenmax)
 - Copy a video link / KeepVid link
 - Copy all links / KeepVid link
 - Display total duration
+- Unit tests!
--- server.js
+++ server.js
@@ -4,13 +4,14 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.listen(port);
+app.use(express.static('app'));
 
-app.get('/', (req, res) => {
+/*app.get('/', (req, res) => {
     res.sendFile('index.html',  {root : __dirname + '/app'});
-});
+});*/
 
 browserSync({
     server: 'app',
-    files: ["app/*.html", "app/css/*.css"],
+    files: ["app/*.html", "app/css/*.css", "app/js/*.js"],
     open: false
 });
