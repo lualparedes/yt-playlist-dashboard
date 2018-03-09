a92918fb139fb2f2dfeda9ba64baebc4f917e4cb
Lual <lualps94@gmail.com>
2018-03-09 18:09:58 -0400

    70% of the basic styles implemented

--- server.js
+++ server.js
@@ -6,10 +6,6 @@ const port = process.env.PORT || 3000;
 app.listen(port);
 app.use(express.static('app'));
 
-/*app.get('/', (req, res) => {
-    res.sendFile('index.html',  {root : __dirname + '/app'});
-});*/
-
 browserSync({
     server: 'app',
     files: ["app/*.html", "app/css/*.css", "app/js/*.js"],
