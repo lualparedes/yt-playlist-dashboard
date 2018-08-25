bec4c99ba615c5282c1489728012f9999b66229e
Lual <lualps94@gmail.com>
2018-08-24 20:59:59 -0400

    Renamed app/ to src/

--- server.js
+++ server.js
@@ -4,10 +4,10 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.listen(port);
-app.use(express.static('app'));
+app.use(express.static('./src'));
 
 browserSync({
-    server: 'app',
-    files: ["app/*.html", "app/css/*.css", "app/js/*.js"],
+    server: './src',
+    files: ["src/*.html", "src/css/*.css", "src/js/*.js"],
     open: false
 });
